405e75e1c633d9dab05b137b87051e98c3b94949
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-14 10:17:40 -0500

    init

---
+++ src/test/java/com/isai/demoinyecciondepencias/app/DemoInyeccionDepenciasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demoinyecciondepencias.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInyeccionDepenciasAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
